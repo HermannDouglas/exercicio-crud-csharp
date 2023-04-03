c9960b643fd858b81fb6c21ab6b60096eaaf0b7e
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2023-04-02 20:27:22 -0500

    Primeiro commit
    Implementado funções para realizar CRUD

---
+++ Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace AppCRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblId = new Label();
+            lblNome = new Label();
+            tbxId = new TextBox();
+            tbxNome = new TextBox();
+            lblSelecionaUsuario = new Label();
+            rbtnAluno = new RadioButton();
+            rbtnProfessor = new RadioButton();
+            rbtnServidor = new RadioButton();
+            btnCadastrar = new Button();
+            btnAtualizar = new Button();
+            btnDeletar = new Button();
+            btnRecarregarDados = new Button();
+            dgvDados = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dgvDados).BeginInit();
+            SuspendLayout();
+            // 
+            // lblId
+            // 
+            lblId.AutoSize = true;
+            lblId.Location = new Point(27, 40);
+            lblId.Name = "lblId";
+            lblId.Size = new Size(20, 15);
+            lblId.TabIndex = 0;
+            lblId.Text = "Id:";
+            // 
+            // lblNome
+            // 
+            lblNome.AutoSize = true;
+            lblNome.Location = new Point(162, 40);
+            lblNome.Name = "lblNome";
+            lblNome.Size = new Size(43, 15);
+            lblNome.TabIndex = 1;
+            lblNome.Text = "Nome:";
+            // 
+            // tbxId
+            // 
+            tbxId.Location = new Point(53, 37);
+            tbxId.Name = "tbxId";
+            tbxId.Size = new Size(100, 23);
+            tbxId.TabIndex = 2;
+            // 
+            // tbxNome
+            // 
+            tbxNome.Location = new Point(211, 37);
+            tbxNome.Name = "tbxNome";
+            tbxNome.Size = new Size(246, 23);
+            tbxNome.TabIndex = 3;
+            // 
+            // lblSelecionaUsuario
+            // 
+            lblSelecionaUsuario.AutoSize = true;
+            lblSelecionaUsuario.Location = new Point(27, 72);
+            lblSelecionaUsuario.Name = "lblSelecionaUsuario";
+            lblSelecionaUsuario.Size = new Size(152, 15);
+            lblSelecionaUsuario.TabIndex = 4;
+            lblSelecionaUsuario.Text = "Selecione o tipo de usuário:";
+            // 
+            // rbtnAluno
+            // 
+            rbtnAluno.AutoSize = true;
+            rbtnAluno.Location = new Point(185, 70);
+            rbtnAluno.Name = "rbtnAluno";
+            rbtnAluno.Size = new Size(57, 19);
+            rbtnAluno.TabIndex = 5;
+            rbtnAluno.TabStop = true;
+            rbtnAluno.Text = "Aluno";
+            rbtnAluno.UseVisualStyleBackColor = true;
+            // 
+            // rbtnProfessor
+            // 
+            rbtnProfessor.AutoSize = true;
+            rbtnProfessor.Location = new Point(248, 70);
+            rbtnProfessor.Name = "rbtnProfessor";
+            rbtnProfessor.Size = new Size(74, 19);
+            rbtnProfessor.TabIndex = 6;
+            rbtnProfessor.TabStop = true;
+            rbtnProfessor.Text = "Professor";
+            rbtnProfessor.UseVisualStyleBackColor = true;
+            // 
+            // rbtnServidor
+            // 
+            rbtnServidor.AutoSize = true;
+            rbtnServidor.Location = new Point(328, 70);
+            rbtnServidor.Name = "rbtnServidor";
+            rbtnServidor.Size = new Size(68, 19);
+            rbtnServidor.TabIndex = 7;
+            rbtnServidor.TabStop = true;
+            rbtnServidor.Text = "Servidor";
+            rbtnServidor.UseVisualStyleBackColor = true;
+            // 
+            // btnCadastrar
+            // 
+            btnCadastrar.Location = new Point(53, 99);
+            btnCadastrar.Name = "btnCadastrar";
+            btnCadastrar.Size = new Size(75, 23);
+            btnCadastrar.TabIndex = 8;
+            btnCadastrar.Text = "Cadastrar";
+            btnCadastrar.UseVisualStyleBackColor = true;
+            btnCadastrar.Click += btnCadastrar_Click;
+            // 
+            // btnAtualizar
+            // 
+            btnAtualizar.Location = new Point(134, 99);
+            btnAtualizar.Name = "btnAtualizar";
+            btnAtualizar.Size = new Size(75, 23);
+            btnAtualizar.TabIndex = 9;
+            btnAtualizar.Text = "Atualizar";
+            btnAtualizar.UseVisualStyleBackColor = true;
+            btnAtualizar.Click += btnAtualizar_Click;
+            // 
+            // btnDeletar
+            // 
+            btnDeletar.Location = new Point(215, 99);
+            btnDeletar.Name = "btnDeletar";
+            btnDeletar.Size = new Size(75, 23);
+            btnDeletar.TabIndex = 10;
+            btnDeletar.Text = "Deletar";
+            btnDeletar.UseVisualStyleBackColor = true;
+            btnDeletar.Click += btnDeletar_Click;
+            // 
+            // btnRecarregarDados
+            // 
+            btnRecarregarDados.Location = new Point(296, 99);
+            btnRecarregarDados.Name = "btnRecarregarDados";
+            btnRecarregarDados.Size = new Size(114, 23);
+            btnRecarregarDados.TabIndex = 11;
+            btnRecarregarDados.Text = "Recarregar dados";
+            btnRecarregarDados.UseVisualStyleBackColor = true;
+            btnRecarregarDados.Click += btnRecarregarDados_Click;
+            // 
+            // dgvDados
+            // 
+            dgvDados.AllowUserToOrderColumns = true;
+            dgvDados.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvDados.Location = new Point(27, 128);
+            dgvDados.Name = "dgvDados";
+            dgvDados.RowTemplate.Height = 25;
+            dgvDados.Size = new Size(430, 297);
+            dgvDados.TabIndex = 12;
+            dgvDados.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(479, 436);
+            Controls.Add(dgvDados);
+            Controls.Add(btnRecarregarDados);
+            Controls.Add(btnDeletar);
+            Controls.Add(btnAtualizar);
+            Controls.Add(btnCadastrar);
+            Controls.Add(rbtnServidor);
+            Controls.Add(rbtnProfessor);
+            Controls.Add(rbtnAluno);
+            Controls.Add(lblSelecionaUsuario);
+            Controls.Add(tbxNome);
+            Controls.Add(tbxId);
+            Controls.Add(lblNome);
+            Controls.Add(lblId);
+            Name = "Form1";
+            Text = "Sistema Acadêmico";
+            ((System.ComponentModel.ISupportInitialize)dgvDados).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblId;
+        private Label lblNome;
+        private Label lblSelecionaUsuario;
+
+        private TextBox tbxId;
+        private TextBox tbxNome;
+
+        private RadioButton rbtnAluno;
+        private RadioButton rbtnProfessor;
+        private RadioButton rbtnServidor;
+
+        private Button btnCadastrar;
+        private Button btnAtualizar;
+        private Button btnDeletar;
+        private Button btnRecarregarDados;
+
+        private DataGridView dgvDados;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,139 @@
+﻿namespace AppCRUD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(tbxId.Text);
+            string nome = tbxNome.Text;
+            string tipo = "Aluno";
+
+            MySqlConnection con = new MySqlConnection(
+            "server = localhost; " +
+            "database = academico; " +
+            "user id = root; " +
+            "password = root;");
+
+            MySqlCommand cmd = new MySqlCommand(
+                "insert into pessoa values ('" + id +
+                "','" + nome +
+                "','" + tipo + "'); "
+                , con);
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+
+            atualizar_ds();
+
+        }
+
+        private void btnAtualizar_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(tbxId.Text);
+            string nome = tbxNome.Text;
+            string tipo = "Aluno";
+
+            MySqlConnection con = new MySqlConnection(
+                "server = localhost; " +
+                "database = academico; " +
+                "user id = root; " +
+                "password = root;");
+
+            MySqlCommand cmd = new MySqlCommand(
+                "update pessoa set nome = '" + nome +
+                "', tipo = '" + tipo +
+                "' where id = '" + id + "' ",
+                con);
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+
+            atualizar_ds();
+        }
+
+        private void btnDeletar_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(tbxId.Text);
+
+
+            MySqlConnection con = new MySqlConnection(
+                "server = localhost; " +
+                "database = academico; " +
+                "user id = root; " +
+                "password = root;");
+
+            MySqlCommand cmd = new MySqlCommand(
+                "delete from pessoa where id = '" + id + 
+                "' ",
+                con);
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+
+            tbxId.Clear();
+            tbxNome.Clear();
+
+            atualizar_ds();
+
+        }
+
+        private void btnRecarregarDados_Click(object sender, EventArgs e)
+        {
+            atualizar_ds();
+        }
+
+        public void atualizar_ds()
+        {
+            MySqlConnection con = new MySqlConnection(
+                "server = localhost; " +
+                "database = academico; " +
+                "user id = root; " +
+                "password = root;");
+
+
+            MySqlCommand cmd = new MySqlCommand(" select * from pessoa ", con);
+
+            con.Open(); // abre a conex�o com o banco
+            cmd.ExecuteNonQuery(); // executa cmd
+            /*Pronto ap�s o cmd.ExecuteNonQuery(); selecionamos tudo o
+            que tinha dentro do banco,
+            agora os passos seguintes ir�o exibir as informa��es para
+            que o usu�rio possa v�-las
+            */
+            MySqlDataAdapter da = new MySqlDataAdapter(); /* da, adapta o banco de dados ao nosso projeto */
+            DataSet ds = new DataSet();
+            da.SelectCommand = cmd; // adapta cmd ao projeto
+            da.Fill(ds); // preenche todas as informa��es dentro do DataSet
+            dgvDados.DataSource = ds; //Datagridview recebe ds j� preenchido
+            dgvDados.DataMember = ds.Tables[0].TableName;
+            /*Agora Datagridview exibe o banco de dados*/
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            tbxId.Text = dgvDados.CurrentRow.Cells[0].Value.ToString();
+            tbxNome.Text = dgvDados.CurrentRow.Cells[1].Value.ToString();
+
+        }
+
+        // Verificar radioButton de tipo de usuário
+        //public string verificaTipoUsuario()
+        //{
+        //    if(rbtnAluno.Checked)
+        //    {
+        //        return "Aluno";
+        //    }else if (rbtnProfessor.Checked)
+        //    {
+        //        return "Professor";
+        //    }else if (rbtnServidor.Checked)
+        //    {
+        //        return "Servidor";
+        //    }
+        //}
+    }
+}
